1d86871c3c145f68d1ed21d49bbe854ebc692fc0
Eazed <Eazed_Yu@outlook.com>
2024-09-28 00:30:12 +0800

    初始化项目

---
+++ eazed-forum-backend/src/test/java/top/eazed/forum/EazedForumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.eazed.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazedForumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
