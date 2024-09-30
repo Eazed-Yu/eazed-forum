675c69c13aaeb083f08adb5efeba16a2a23ddc9f
Eazed <Eazed_Yu@outlook.com>
2024-09-30 20:54:38 +0800

    用户个人信息第一部分完成

--- eazed-forum-frontend/src/views/settings/UserSetting.vue
+++ eazed-forum-frontend/src/views/settings/UserSetting.vue
@@ -3,9 +3,85 @@
 import Card from "@/components/Card.vue";
 import {Message, Refresh, Select, User} from "@element-plus/icons-vue";
 import {useStore} from "@/store/index.js";
-import {computed} from "vue";
+import {computed, reactive, ref} from "vue";
+import {get, post} from "@/net/index.js";
+import {ElMessage} from "element-plus";
 const store = useStore()
 const registerTime = computed(() => new Date(store.user.registerTime).toLocaleString())
+
+
+const description = ref('')
+const baseFormRef = ref()
+const emailFormRef = ref()
+
+const baseForm = reactive({
+  username: '',
+  gender: 1,
+  phone: '',
+  qq: '',
+  wechat: '',
+  description: ''
+})
+const emailFrom = reactive({
+  email: '',
+  code: ''
+})
+const validateUsername = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请输入用户名'))
+  } else if (!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)) {
+    callback(new Error('用户名只能包含字母、数字和汉字'))
+  } else {
+    callback()
+  }
+}
+const rules = {
+  username: [
+    {validator: validateUsername, trigger: ['blur', 'change']},
+    {min: 1, max: 10, message: '用户名长度在 1 到 10 个字符', trigger: 'blur'}
+  ],
+  email: [
+    {required: true, message: '请输入电子邮件', trigger: ['blur', 'change']},
+    {type: 'email', message: '请输入正确的电子邮件', trigger: ['blur', 'change']}
+  ],
+  code: [
+    {required: true, message: '请输入验证码', trigger: ['blur', 'change']},
+    {min: 6, max: 6, message: '验证码长度为6位', trigger: ['blur', 'change']}
+  ]
+}
+
+function saveDetails() {
+  baseFormRef.value.validate((valid) => {
+    if (valid) {
+      loading.base = true
+      post('/api/user/save-details', baseForm, () => {
+        ElMessage.success('保存成功')
+        loading.base = false
+        store.user.username = baseForm.username
+        description.value = baseForm.description
+      }, message => {
+        loading.base = false
+        ElMessage(message)
+      })
+    }
+  })
+}
+
+const loading = reactive({
+  form: true,
+  base: false
+})
+
+
+get('api/user/details', data => {
+  baseForm.username = store.user.username
+  baseForm.gender = data.gender
+  baseForm.phone = data.phone
+  baseForm.qq = data.qq
+  baseForm.wechat = data.wechat
+  baseForm.description = data.description
+  loading.form = false
+})
 </script>
 
 <template>
@@ -14,33 +90,34 @@ const registerTime = computed(() => new Date(store.user.registerTime).toLocaleSt
       <div class="cards">
         <card :icon="User"
               class="card"
+              v-loading="loading.form"
               description="在这里编辑您的个人信息，您可以在隐私设置中选择是否展示这些信息"
               title="账号信息设置">
-          <el-form label-position="top" style="margin: 0 10px 10px 10px">
+          <el-form ref="baseFormRef" :model="baseForm" :rules="rules" label-position="top" style="margin: 0 10px 10px 10px">
             <el-form-item label="用户名">
-              <el-input></el-input>
+              <el-input v-model="baseForm.username"></el-input>
             </el-form-item>
             <el-form-item label="性别">
-              <el-radio-group>
-                <el-radio value="1">男</el-radio>
-                <el-radio value="2">女</el-radio>
+              <el-radio-group v-model="baseForm.gender">
+                <el-radio :value="0">男</el-radio>
+                <el-radio :value="1">女</el-radio>
               </el-radio-group>
             </el-form-item>
-            <el-form-item label="手机号">
-              <el-input></el-input>
+            <el-form-item v-model="baseForm.phone" label="手机号" prop="phone">
+              <el-input v-model="baseForm.phone" maxlength="11"/>
             </el-form-item>
 
-            <el-form-item label="qq号">
-              <el-input></el-input>
+            <el-form-item label="qq号" prop="qq">
+              <el-input v-model="baseForm.qq" maxlength="13"/>
             </el-form-item>
-            <el-form-item label="微信号">
-              <el-input></el-input>
+            <el-form-item label="微信号" prop="wechat">
+              <el-input v-model="baseForm.wechat" maxlength="20"/>
             </el-form-item>
-            <el-form-item label="个人简介">
-              <el-input :rows="6" type="textarea"></el-input>
+            <el-form-item label="个人简介" prop="description">
+              <el-input v-model="baseForm.description" :rows="6" maxlength="200" type="textarea"/>
             </el-form-item>
             <div>
-              <el-button :icon="Select" type="success">保存用户信息</el-button>
+              <el-button :icon="Select" :loading="loading.base" type="success" @click="saveDetails">保存用户信息</el-button>
             </div>
           </el-form>
         </card>
@@ -48,14 +125,14 @@ const registerTime = computed(() => new Date(store.user.registerTime).toLocaleSt
               class="card"
               description="您可以修改绑定的电子邮件地址" style="margin-top: 10px"
               title="电子邮件设置">
-          <el-form label-position="top" style="margin: 0 10px 10px 10px">
-            <el-form-item label="电子邮件">
-              <el-input></el-input>
+          <el-form ref="emailFormRef" :model="emailFrom" :rules="rules" label-position="top" style="margin: 0 10px 10px 10px">
+            <el-form-item label="电子邮件" prop="email">
+              <el-input v-model="emailFrom.email"/>
             </el-form-item>
-            <el-form-item>
+            <el-form-item prop="code">
               <el-row :gutter="10" style="width: 100%;">
                 <el-col :span="18">
-                  <el-input placeholder="请输入验证码"/>
+                  <el-input v-model="emailFrom.code" placeholder="请输入验证码"/>
                 </el-col>
                 <el-col :span="6">
                   <el-button type="success">获取验证码</el-button>
@@ -79,9 +156,7 @@ const registerTime = computed(() => new Date(store.user.registerTime).toLocaleSt
             </div>
             <el-divider style="margin: 10px 0"/>
             <div class="user-introduction">
-              个人信息介绍
-              阿斯顿发大水发射点发射点发射点发射点发
-              阿斯蒂芬阿斯顿发顺丰阿斯顿富士达法撒旦发大水f大师傅发送十大发
+              {{description || "这个人很懒，什么都没留下"}}
             </div>
           </div>
         </card>
