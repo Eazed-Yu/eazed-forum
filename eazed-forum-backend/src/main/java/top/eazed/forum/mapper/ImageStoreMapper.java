299919536177a0c6de046bbb233c07b10446f122
Eazed <Eazed_Yu@outlook.com>
2024-10-08 21:13:07 +0800

    修复图片上传下载后端bug

--- eazed-forum-backend/src/main/java/top/eazed/forum/mapper/ImageStoreMapper.java
+++ eazed-forum-backend/src/main/java/top/eazed/forum/mapper/ImageStoreMapper.java
@@ -1,7 +1,9 @@
 package top.eazed.forum.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import top.eazed.forum.entity.dto.StoreImageDTO;
 
+@Mapper
 public interface ImageStoreMapper extends BaseMapper<StoreImageDTO> {
 }
